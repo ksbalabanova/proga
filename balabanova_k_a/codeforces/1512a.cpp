3ff62388dc1c48141c1fd8aaade9adfaca0178b0
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-11 13:36:06 +0300

    Create 1512a.cpp

---
+++ balabanova_k_a/codeforces/1512a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (t; t > 0; t--) {
+        int n;
+        std::cin >> n;
+        std::vector <int> a{ 0 };
+        for (int i = 1; i < n + 1; i++) {//ввели массив
+            std::cin >> a[i];
+        }
+        int b;
+        for (int i = 1; i < n + 1; i++) {
+            if (a[i] != a[i + 1] && a[i] != a[i + 2]) {
+                a[i] = 0;
+                break;
+            }
+            if (a[i] != a[i + 1] && a[i] == a[i + 2]) {
+                a[i + 1] = 0;
+                break;
+            }
+            if (a[n] != a[n - 1] && a[n - 1] != 0) {
+                a[n] = 0;
+                break;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            if (a[i] == 0) {
+                std::cout << i << std::endl;
+            }
+        }
+    }
+}
