9dd780045772ad76cae5725d098207e88cd57792
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-11 15:47:29 +0300

    Create 1680b.cpp

---
+++ balabanova_k_a/codeforces/1680b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+    int t;
+    std::cin >> t;
+    for (t; t >= 0; t--){
+        int n, m;
+        std::cin >> n >> m >> std::endl;
+        char arr [n][m];
+        for (n; n >= 0; n--){
+            std::cout << std::endl;
+            for (m; m >= 0; m--){
+                std::cin >> a[n][m];
+            }
+        }
+        int nmax = -10;//чтобы найти индексы крайнего робота
+        int mmax = -10;
+    }
+    
+    
+    
+    
+}
