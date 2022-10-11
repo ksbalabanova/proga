6ac2d80dc99f94552b65f492a17173f40d049cad
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-11 15:45:57 +0300

    Create 1671a.cpp

---
+++ balabanova_k_a/codeforces/1671a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    int t;
+    std::cin >> t;
+    std::cout << std::endl;
+    for (t; t >= 0; t--){
+        std::string s;
+        getline (std::cin, s);
+        if ()
+    }
+}
