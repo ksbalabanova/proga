750df241be36c5ec04a6f67bc1496c1619f26afc
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-11 15:45:01 +0300

    Create 0915a.cpp

---
+++ balabanova_k_a/codeforces/0915a.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+int main (){
+    int n, k;
+    std::cin >> n >> k;
+    std::vector <int> arr{0};
+    for(int i = 0; i < n; i++){
+        std::cin >> arr[i];
+    }
+    int nmax = 0, l; 
+    
+    for (int i = 0; i < n; i++){
+        if (k % arr[i] == 0 && arr[i] > nmax){
+            nmax = arr[i];
+        }
+    }
+    l = k / nmax;
+    std::cout << l;
+    return 0;
+}
