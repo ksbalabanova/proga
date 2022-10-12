dab332ccaa9842b04306492efbe6de038db3e4bb
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-13 02:34:32 +0300

    Update 1680b.cpp

--- balabanova_k_a/codeforces/1680b.cpp
+++ balabanova_k_a/codeforces/1680b.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 int main(){
     int t;
@@ -20,4 +20,48 @@ int main(){
     
     
     
+}*/
+
+#include <iostream> 
+#include <vector>
+
+int main(){
+      int m, n;
+      std::cin >> n >> m;
+      std::vector < std::vector <char> > arr(n, std::vector <char> (m) );
+      for (int i = 0; i < n; i++){//ввод массива
+          for (int j = 0; j < m; j++){
+              std::cin >> arr[i][j];
+          }
+      }
+      // in, im - индекс самого близкого к краю робота
+      int in = 10, im = 10;
+      for (int i = 0; i < n; i++){//обработка
+          for (int j = 0; j < m; j++){//запомнили индексы самого близкого к краю робота
+              if (arr[i][j] == 'R' && i < in && j < im){
+                  in = i;
+                  im = j;
+              }
+              
+          }
+      }
+      /*for (int i = 0; i < n; i++){//обработка
+          for (int j = 0; j < m; j++){
+              int dmin, d;
+              dmin = in * in + im * im;
+              d = i * i + j * j;
+              if (d < dmin and arr [i][j] == 'R'){
+                  std::cout << "NO";
+                  i = n;
+                  j = m;
+                  break;
+              }else {
+                  std::cout << "NO";
+                  i = n;
+                  j = m;
+                  break;
+              }
+              
+          }
+      }*/
 }
