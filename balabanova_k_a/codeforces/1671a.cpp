3a5df325be4d3a8ec67dd204e96c0bd11422ef53
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-13 01:09:23 +0300

    Update 1671a.cpp

--- balabanova_k_a/codeforces/1671a.cpp
+++ balabanova_k_a/codeforces/1671a.cpp
@@ -2,12 +2,36 @@
 #include <string>
 
 int main(){
-    int t;
+    /*int t;
     std::cin >> t;
-    std::cout << std::endl;
-    for (t; t >= 0; t--){
+    std::cout <<std::endl;
+    std::string s;
+    while (t > 0){
+        t--;*/
         std::string s;
         getline (std::cin, s);
-        if ()
-    }
+        if (s.length() == 1){
+            std::cout << "NO";
+            //break;
+        }else{
+            for (int i = 1; i < s.length() - 1; i++){
+                if (s[0] != s[1]){
+                    std::cout << "NO" << std::endl;
+                    break;
+                }
+                if (s[s.length() - 1] != s[s.length() - 2]){
+                    std::cout << "NO" << std::endl;
+                    break;
+                }
+                if(s[i] != s[i - 1] && s[i] != s[i + 1]){
+                    std::cout << "NO" << std::endl;
+                    std::cout << s[i];
+                    break;
+                } else {
+                    std::cout << "YES" <<std::endl;
+                    break;
+                }
+            }
+        }
+    //}
 }
