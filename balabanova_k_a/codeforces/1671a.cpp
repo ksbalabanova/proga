2d4e6d51ba3202bec396a0826985e08f794a8ccf
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-13 09:50:03 +0300

    Update 1671a.cpp

--- balabanova_k_a/codeforces/1671a.cpp
+++ balabanova_k_a/codeforces/1671a.cpp
@@ -1,37 +1,39 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 
 int main(){
-    /*int t;
+    int t;
     std::cin >> t;
-    std::cout <<std::endl;
-    std::string s;
-    while (t > 0){
-        t--;*/
+    for (t; t >= 0; t--){
         std::string s;
-        getline (std::cin, s);
-        if (s.length() == 1){
-            std::cout << "NO";
-            //break;
-        }else{
-            for (int i = 1; i < s.length() - 1; i++){
-                if (s[0] != s[1]){
-                    std::cout << "NO" << std::endl;
-                    break;
-                }
-                if (s[s.length() - 1] != s[s.length() - 2]){
-                    std::cout << "NO" << std::endl;
-                    break;
-                }
-                if(s[i] != s[i - 1] && s[i] != s[i + 1]){
-                    std::cout << "NO" << std::endl;
-                    std::cout << s[i];
-                    break;
-                } else {
-                    std::cout << "YES" <<std::endl;
-                    break;
+        while (t > 0){
+            t--;
+            std::string s;
+            getline (std::cin, s);
+            std::cout << std::endl;
+            if (s.length() == 1){
+                std::cout << "NO";
+                break;
+            }else{
+                for (int i = 1; i < s.length() - 1; i++){
+                    if (s[0] != s[1]){
+                        std::cout << "NO" << std::endl;
+                        break;
+                    }
+                    if (s[s.length() - 1] != s[s.length() - 2]){
+                        std::cout << "NO" << std::endl;
+                        break;
+                    }
+                    if(s[i] != s[i - 1] && s[i] != s[i + 1]){
+                        std::cout << "NO" << std::endl;
+                        std::cout << s[i];
+                        break;
+                    } else {
+                        std::cout << "YES" <<std::endl;
+                        break;
+                    }
                 }
             }
         }
-    //}
+    }
 }
