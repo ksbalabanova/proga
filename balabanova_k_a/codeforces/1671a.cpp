53952c3c6950e08dc8b22360ded9dd38764a2180
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-17 23:47:13 +0300

    Update 1671a.cpp

--- balabanova_k_a/codeforces/1671a.cpp
+++ balabanova_k_a/codeforces/1671a.cpp
@@ -1,38 +1,34 @@
 #include <string>
 #include <iostream>
-
+ 
 int main(){
   int t;
-  //std::cout << "Введите кол-во наборов:" << std::endl;
   std::cin >> t;
-    for (t; t > 0; t--){
-      //std::cout << "Введите строку s:" << std::endl;
-      std::string s;
-      getline (std::cin, s);
-      //std::cout << s.length();
-      if (s.size() == 1){
-        std::cout << "NO1";
-        break;
+  for (t; t > 0; t--){
+    std::string s = "";
+    std::cin >> s;
+    int h = 0;
+    int k = 0;
+    for (int i = 0; i < s.length(); i++){
+      if(s[i] == s[i + 1]){
+        k = k + 1;
       } else {
-          for (int i = 1; i < s.length() - 1; i++){
-            if (s[0] != s[1]){
-              std::cout << "NO2" << std::endl;
-              break;
-            }
-            if (s[s.length() - 1] != s[s.length() - 2]){
-              std::cout << "NO3" << std::endl;
-              break;
-            }
-            if(s[i] != s[i - 1] && s[i] != s[i + 1]){
-              std::cout << "NO4" << std::endl;
-              std::cout << s[i];
-              break;
-            } else {
-                std::cout << "YES" <<std::endl;
-                break;
-              }
-          }
+        if(k > 1){
+          h = 1;
+          k = 1;
+        }else{
+          h = 0;
+          k = 1;
+          break;
         }
+      }
     }
-
+    if (h == 1){
+      std::cout << "YES" << std::endl;
+    }else{
+      std::cout << "NO" << std::endl;
+    }
+    h = 0;
+    k - 0;
+  }
 }
