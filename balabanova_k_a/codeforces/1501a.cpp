3a543395dc9c525ee70643a9cef017c047bbb85d
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-10-23 23:57:04 +0300

    Create 1501a.cpp

---
+++ balabanova_k_a/codeforces/1501a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (t; t > 0; t--) {
+        int n;
+        std::cin >> n;
+        std::vector <int> a;
+        std::vector <int> b;
+        std::vector <int> tm;
+        for (int i = 0; i < n; i++) {
+            std::cin >> a[i] >> b[i];
+        }
+        for (int i = 0; i < n; i++) {
+            std::cin >> tm[i];
+        }
+        int all_time = 0;
+        int delta = 0;
+        for (int i = 0; i < n; i++) {
+            all_time = all_time + (a[i] - delta + tm[i]);
+            int stop_time = (b[i] - a[i] + 1) / 2;
+            if (i < n - 1) {
+                all_time = all_time + stop_time;
+            }
+            if (all_time < b[i] && i < n - 1) {
+                all_time = b[i];
+            }
+            delta = b[i];
+        }
+        std::cout << all_time << std::endl;
+    }
+    return 0;
+}
