3df2007270ff5fa357597b07bfd869ecb70afa80
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-09 11:14:51 +0300

    Update 0915a.cpp

--- balabanova_k_a/codeforces/0915a.cpp
+++ balabanova_k_a/codeforces/0915a.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
 
-int main (){
+int main() {
     int n, k;
     std::cin >> n >> k;
-    std::vector <int> arr{0};
+    std::vector <int> arr;
     arr.resize(n);
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         std::cin >> arr[i];
     }
-    int nmax = 0, l; 
-    
-    for (int i = 0; i < n; i++){
-        if (k % arr[i] == 0 && arr[i] > nmax){
+    int nmax = 0, l;
+    for (int i = 0; i < n; i++) {
+        if (k % arr[i] == 0 && arr[i] > nmax) {
             nmax = arr[i];
         }
     }
