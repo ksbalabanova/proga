be2c6456665ef6f663ee9a0bfc0cd907fc6f6c1b
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-10 00:09:35 +0300

    Update 1671a.cpp

--- balabanova_k_a/codeforces/1671a.cpp
+++ balabanova_k_a/codeforces/1671a.cpp
@@ -1,34 +1,39 @@
-#include <string>
 #include <iostream>
- 
-int main(){
-  int t;
-  std::cin >> t;
-  for (t; t > 0; t--){
-    std::string s = "";
-    std::cin >> s;
-    int h = 0;
-    int k = 0;
-    for (int i = 0; i < s.length(); i++){
-      if(s[i] == s[i + 1]){
-        k = k + 1;
-      } else {
-        if(k > 1){
-          h = 1;
-          k = 1;
-        }else{
-          h = 0;
-          k = 1;
-          break;
+#include <string>
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (t; t > 0; t--) {
+        std::string s;
+        std::cin >> s;
+        if (s.size() == 1) {
+            std::cout << "NO" << std::endl;
         }
-      }
+        if (s.size() == 2) {
+            if (s[0] == s[1]) {
+                std::cout << "YES" << std::endl;
+            }
+            if (s[0] != s[1]) {
+                std::cout << "NO" << std::endl;
+            }
+
+        }
+        int j = 0;
+        int tmp = 0;
+        if (s.size() > 2) {
+            while (j + 2 < s.size()) {
+                if ((s[0] != s[1]) || (s[j] != s[j + 1] && s[j + 1] != s[j + 2]) || (s[s.size() - 2] != s[s.size() - 1])) {
+                    std::cout << "NO" << std::endl;
+                    tmp = 1;
+                    break;
+                }
+                j++;
+            }
+            if (tmp == 0) {
+                std::cout << "YES" << std::endl;
+            }
+        }
     }
-    if (h == 1){
-      std::cout << "YES" << std::endl;
-    }else{
-      std::cout << "NO" << std::endl;
-    }
-    h = 0;
-    k - 0;
-  }
+    return 0;
 }
