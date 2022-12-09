98d2be549eb7bf9179f91fe0e28afcfe47d907ab
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-09 22:42:05 +0300

    Update 0136a.cpp

--- balabanova_k_a/codeforces/0136a.cpp
+++ balabanova_k_a/codeforces/0136a.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <vector>
- 
-int main(){
-  int n;
-  std::cin >> n;
-  std::vector <int> ktokomy {0};
- ktokomy.resize(n);
-  std::vector <int> ktoot {0};
- ktoot.resize(n);
-  for (int i = 1; i < n + 1; i++){
-    std::cin >> ktokomy[i];
-  }
-  for (int i = 1; i < n + 1; i++){
-    int m, l;
-    m = ktokomy[i];
-    l = i;
-    ktoot [m] = l;
-  }
-  for (int i = 1; i < n + 1; i++){
-    std::cout << ktoot[i] << ' ';
-  }
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector <int> arr1;
+    arr1.resize(n + 1);
+    std::vector <int> arr2;
+    arr2.resize(n + 1);
+    for (int i = 1; i < n + 1; i++) {
+        std::cin >> arr1[i];
+    }
+    for (int i = 1; i < n + 1; i++) {
+        int m, l;
+        m = arr1[i];
+        l = i;
+        arr2[m] = l;
+    }
+    for (int i = 1; i < n + 1; i++) {
+        std::cout << arr2[i] << ' ';
+    }
+    return 0;
 }
