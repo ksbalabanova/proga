bdafcdd5894dc4fad31fdf9a66974ccea75ac4f2
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-09 22:31:49 +0300

    Update 1649a.cpp

--- balabanova_k_a/codeforces/1649a.cpp
+++ balabanova_k_a/codeforces/1649a.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 #include <vector>
- 
-int main(){
-  int t;
-  std::cin >> t;
-  for (t; t >= 0; t--){
-    int n;
-    std::cin >> n;//кол-во локаций
-    std::vector <int> a{2};
-    for (int i = 1; i < n + 1; i ++){
-      std::cin >> a[i];
-    }//проверила все до этой строки
-    int x = 0; //счетчик монет
-    int ferst = 100, last = - 1; //индексы первого и последнего нуля
-    for (int i = 1; i < n + 1; i ++){
-      if (a[i] == 0 && i < ferst){
-        ferst = i - 1;
-      }
-      if (a[i] == 0 && i > last){
-        last = i + 1;
-      }
-    } //индексы выводит правильно
-    if (last != - 1 && ferst != 100){
-      x = last - ferst;
-      std::cout << x;
-    } else {
-      std::cout << x;
+
+int main() {
+    int t;
+    std::cin >> t;
+    for (t; t > 0; t--) {
+        int n;
+        std::cin >> n;
+        std::vector <int> a;
+        a.resize(n);
+        int last = -1;
+        int first;
+        for (int i = 0; i < n; i++) {
+            std::cin >> a[i];
+            if (last == -1 && a[i] == 0) {
+                last = i;
+            }
+            if (a[i] == 0) {
+                first = i;
+            }
+        }
+        if (last == -1) {
+            std::cout << 0 << std::endl;
+        }
+        else {
+            std::cout << first - last + 2 << std::endl;
+        }
     }
-  }
+    return 0;
 }
