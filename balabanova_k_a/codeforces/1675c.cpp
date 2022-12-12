38562bea2a94dcae2daf797880e952b1379a69d1
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-12 10:46:37 +0300

    Update 1675c.cpp

--- balabanova_k_a/codeforces/1675c.cpp
+++ balabanova_k_a/codeforces/1675c.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-//нужно посмотреть, что происходит если длина равна 1, если все вопросы
 
 int main() {
     int t;
@@ -8,27 +7,42 @@ int main() {
     for (t; t > 0; t--) {
         std::string s;
         std::cin >> s;
+        int count = 0;
         int ans;
-        int index0 = -1, index1 = -1;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '0') {
-                index0 = i;
-                break;
+            if (s[i] == '?') {
+                count = count + 1;
             }
         }
-        if (index0 == -1) {
-            index0 = 1;
+        if (count == s.size()) {
+            ans = count;
         }
-        for (int i = s.size() - 1; i >= 0; i--) {
-            if (s[i] == '1') {
-                index1 = i;
-                break;
+        else if (s.size() == 1) {
+            ans = 1;
+        }
+        else {
+            int index0 = -1, index1 = -1;
+            for (int i = 0; i < s.size(); i++) {
+                if (s[i] == '0') {
+                    index0 = i;
+                    break;
+                }
             }
+            if (index0 == -1) {
+                index0 = 1;
+            }
+            for (int i = s.size() - 1; i >= 0; i--) {
+                if (s[i] == '1') {
+                    index1 = i;
+                    break;
+                }
+            }
+            if (index1 == -1) {
+                index1 = 0;
+            }
+            ans = std::abs(index0 - index1 + 1);
         }
-        if (index1 == -1) {
-            index1 = 0;
-        }
-        ans = index0 - index1 + 1;
-        std::cout << ans;
+        std::cout << ans << std::endl;;
     }
+    return 0;
 }
