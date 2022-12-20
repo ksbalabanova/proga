69e683328c6c6ec322235c8a646746bc60197c3d
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-21 02:14:55 +0300

    Update bpm_22_1_1.cpp

--- balabanova_k_a/bpm_22_1_1.cpp
+++ balabanova_k_a/bpm_22_1_1.cpp
@@ -2,15 +2,20 @@
 #include <cmath>
 
 int main() {
-    double eps = 0.0001, delta_x = 0.05, sum = 0, chlen, i = 1;
-    std::cout << 'x' << '   ' << 'sum' << std::endl;
+    double eps = 0.0001, delta_x = 0.05, sum = 0, chlen = 0, i = 1, tmp = 0;
+    std::cout << 'x' << "    " << "sum" << std::endl;
     for (double x = 0.1; x <= 1; x += 0.05) {
         do {
-            sum = sum + chlen;
             chlen = (std::pow(-1, i + 1) * std::pow(x, 2 * i + 1) )/ (4 * i * i - 1);
+            tmp += chlen;
             i++;
         } while (std::abs(chlen) >= eps);
-            std::cout << x << '  ' << sum << std::endl;
+        sum += tmp;
+            std::cout << x << "    " << sum << std::endl;
+        sum = 0;
+        chlen = 0;
+        i = 0;
+        tmp = 0;
     }
     return 0;
 }
