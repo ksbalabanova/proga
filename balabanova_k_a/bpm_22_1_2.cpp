632ecadc1c55a1f06782689a87293f094f26f507
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-20 15:57:28 +0300

    Create bpm_22_1_2.cpp

---
+++ balabanova_k_a/bpm_22_1_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    double x, y;
+    int count_all = 0;
+    int count = 0;
+    double ans;
+    while () {//не знаю как читать из потока
+        if (((x * x) + (y * y) <= 2) && ((y + x * x )>= 2)) {
+            count++;
+        }
+        count_all++;
+    }
+    ans = count / count_all;
+    std::cout << ans;
+}
