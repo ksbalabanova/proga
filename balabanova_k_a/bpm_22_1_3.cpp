f00c72f3266dd22842e13b9b1695cfd76c6a7a5a
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2022-12-21 01:56:57 +0300

    Update bpm_22_1_3.cpp

--- balabanova_k_a/bpm_22_1_3.cpp
+++ balabanova_k_a/bpm_22_1_3.cpp
@@ -7,31 +7,36 @@ int main() {
     std::cin >> s;
     std::cin >> n;
     int tmp = 0;
-    if (s[s.size() - 1] == n) {
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == 'a' || s[i] == 'o' || s[i] == 'u' || s[i] == 'i' || s[i] == 'e') {
-                s[i] = '1';
-            }
-            else if (s[i] == n) {
-                s[i] = n;
-            }
-            else {
-                s[i] = '0';
-            }
-            int j = 0;
-            while (j < (s.size() - 1)) {
-                if (s[j] + s[j + 1] != '00') {
-                    tmp++;
-                }
-            }
-            if (tmp == s.size() - 1) {
-            std::cout << "YES"
-            }
-            else {
-            std::cout << "NO";
-            }
+    int count_n = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'a' || s[i] == 'o' || s[i] == 'u' || s[i] == 'i' || s[i] == 'e') {
+            s[i] = '1';
+        }
+        else if (s[i] == n) {
+            s[i] = n;
+        }
+        else {
+            s[i] = '0';
+        }
+    }
+    int j = 0;
+    while (j < (s.size() - 1)) {
+        if ((s[j] == '0' && s[j + 1] == '1') || (s[j] == '1' && s[j + 1] == '1')  || (s[j] == '1' && s[j + 1] == '0')) {
+            tmp++;
+            j+=2;
+        }
+        else if(s[i] == n){
+            j++;
+            count_n++;
+        }
+        else{
+        break;
+        }
+    }
+    if (tmp  + count_n == (s.size() + count_n * 2 - 1) / 2) {
+        std::cout << "YES";
     }
     else {
         std::cout << "NO";
-    }
+    }   
 }
